c0c6b5f72c482983acbfd5e8e87f988f5692d37c
crepybozz <mungthomte@gmail.com>
2025-01-27 21:38:00 +0530

    Advertisement.jsx

--- src/components/Advertisement.jsx
+++ src/components/Advertisement.jsx
@@ -17,10 +17,11 @@ const Advertisement = () => {
 
           {active ? (
                      <a href='./images/NEET.pdf' download="NEET_Registration.pdf">
-                              <div className="col">NEET 2025 Registration Starting Soon <button style={{margin: '0 0 0 0px'}}>Apply Now</button></div> </a> ):(
+                              <div className="col">NEET 2025 Registration Starting Soon</div> </a> ):(
           <NavLink to={'https://upsconline.gov.in/upsc/OTRP/index.php'}>
-            <div className="col">UPSC CSC 2025 Registration Opens<button style={{margin: '0 0 0 40px'}}>Apply Now</button></div></NavLink>
+            <div className="col">UPSC CSC 2025 Registration Opens</div></NavLink>
           )}
+           <button>Apply Now</button>
                               {/* <div><img src="./images/hot-news.gif" alt="" /></div> */}
 
     </div>
