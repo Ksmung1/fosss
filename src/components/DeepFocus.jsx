1590d9c668317a7d1cb600490db7db58c2fc51ab
crepybozz <mungthomte@gmail.com>
2025-01-28 20:18:19 +0530

    main

--- src/components/DeepFocus.jsx
+++ src/components/DeepFocus.jsx
@@ -1,30 +1,33 @@
 import React from 'react'
 
 const DeepFocus = () => {
-          const defaultMessage = encodeURIComponent("")
+          // const defaultMessage = encodeURIComponent("")
   return (
     <div className='deepfocus mid flex' >
       
        <ul>
-          <li>
-          <div>
+       <div>
           <h1>CLASS X</h1>
           <h2>REVISION PROGRAM</h2>
           </div>
+          <li>
+      
+      <div className="col">
+                <p>Smart Mind Mapping</p>
+      </div>
+</li>
+          <li>
                     <div className="col">
-                              <p>Smart Mind Mapping</p>
+                              <p>NCERT Questions</p>
                     </div>
           </li>
+        
           <li>
                     <div className="col">
                               <p>Sample Questions</p>
                     </div>
           </li>
-          <li>
-                    <div className="col">
-                              <p>Assertions - Reasons</p>
-                    </div>
-          </li>
+         
           <li>
                     <div className="col">
                               <p><span style={{color:'red'}}>HOTS Question</span></p>
@@ -35,18 +38,17 @@ const DeepFocus = () => {
                               <p>Competency Based Questions</p>
                     </div>
           </li>
-          <li>
-                    <div className="col">
-                              <p>NCERT</p>
-                    </div>
-          </li>
+         
           <li style={{marginTop: '10px'
           }}>
                     <div className="col">
-                              <button style={{cursor: 'pointer'}} onClick={()=> window.open(`https://wa.me/+918974808279?text=${defaultMessage}`)}>Join our Community</button>
+                              <button style={{cursor: 'pointer'}} onClick={()=> window.open(`https://chat.whatsapp.com/Juk5SFpnhAGBLfjV74lOZu`)}>Join our Community</button>
                     </div>
           </li>
        </ul>
+       {/* <div className="image-container">
+          <img src="./images/megaphone.gif" alt="" />
+       </div> */}
     </div>
   )
 }
